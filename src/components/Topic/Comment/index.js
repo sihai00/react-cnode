9e81bfe579dc779f71005af78f96c09646679c90
sihai <464990030@qq.com>
2017-06-04 22:42:06 +0800

    fix

--- src/components/Topic/Comment/index.js
+++ src/components/Topic/Comment/index.js
@@ -5,24 +5,23 @@ class Comment extends Component {
   constructor(content,props){
     super(content, props)
     this.state = {
-      txt: ''
+      txt: this.props.reply_id ? `@${this.props.replyName} ` : ''
     }
   }
   handleClick(){
     this.props.handleClick(this.state.txt, this.props.topic_id, this.props.reply_id)
 
-    this.props.changeInputId('')
+    this.props.changeInputId && this.props.changeInputId('')
     this.setState({
       txt: ''
     })
   }
   render() {
-    let { style, reply_id, replyName } = this.props
-    reply_id = reply_id ? reply_id : ''
+    let { style } = this.props
     return (
       <div className="comment-textarea" style={style}>
         <form>
-          <textarea className="form-control" rows="3" onChange={e => this.setState({txt: e.target.value})} defaultValue={reply_id ? `@${replyName} ` : ''}></textarea>
+          <textarea className="form-control" rows="3" onChange={e => this.setState({txt: e.target.value})} value={this.state.txt}></textarea>
           <button type="button" className="btn btn-success" onClick={this.handleClick.bind(this)}>提交</button>
         </form>
       </div>
